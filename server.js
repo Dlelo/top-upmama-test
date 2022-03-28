9230d32ea64f370ad44b6657e1075d11cffe6a4d
donattah <akinyidonattah@gmail.com>
2022-03-28 16:39:28 +0300

    core(feat): configure and deploy to heroku

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/topupmamatest'));
-app.get('/*', function(req, res) {
+app.use(express.static(__dirname + '/topupmamatest/dist/'));
+app.get('*', function(req, res) {
     res.sendFile(path.join(__dirname +
-        '/dist/topupmamatest/index.html'));
+        '/topupmamatest/dist/index.html'));
 });
 app.listen(process.env.PORT || 8080);
